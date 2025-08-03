894060c07933ea9e3d41b787ce65325ab05e3711
Skioulis <skioulis@gmail.com>
2025-08-03 11:29:25 +0300

    render multiple notes dynamically using props and map function; add notes data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Note from './Note.jsx';
+import notes from '../notes.js';
+import note from "./Note.jsx";
 
 function App() {
     return (
         <>
             <Header/>
-            <Note/>
+            {notes.map(note => <Note key={note.key} title={note.title} content={note.content}/>)}
             <Footer/>
 
         </>
